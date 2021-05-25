0dbeb495a5efe8b9cc4930f7a1bc8b071eaf2c06
cysonguyen <=>
2021-05-25 09:34:20 +0700

    First

---
+++ src/test/java/com/example/projectii/ProjectIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
